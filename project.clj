28e17156e5ed6a6d0e75701ce42d91dbe08aa2ce
ryan spangler <ryan.spangler@gmail.com>
2012-06-21 15:01:15 -0700

    project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject antler/xyzzy "0.1.0"
-  :description "XYZZY: magic clojure xml handling"
+  :description "XYZZY: magical clojure xml handling"
   :url "http://github.com/antler/xyzzy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
